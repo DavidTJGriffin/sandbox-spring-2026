c0d0f3157e2edd63b32ba7e569bf486ed2985d2c
DavidTJGriffin <david.dtjg2001@gmail.com>
2026-02-12 18:20:06 -0700

    updated

--- src/main/java/org/example/sandbox/cards/StandardDeck.java
+++ src/main/java/org/example/sandbox/cards/StandardDeck.java
@@ -1,4 +1,58 @@
 package org.example.sandbox.cards;
 
-public class StandardDeck {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StandardDeck implements Deck {
+
+    List<Card> cards;
+
+    public StandardDeck() {
+        this.cards = newDeck();
+    }
+
+    private List<Card> newDeck() {
+        List<Card> cards = new ArrayList<>();
+
+        Arrays.stream(Suit.values()).forEach(suit -> {
+            Arrays.stream(FaceValue.values()).map(faceValue -> new Card(suit, faceValue)).forEach(cards::add);
+        });
+        return cards;
+    }
+
+    @Override
+    public void shuffle() {
+
+    }
+
+    @Override
+    public void cut(int index) {
+
+    }
+
+    @Override
+    public Card deal() {
+        return null;
+    }
+
+    @Override
+    public Card turnOver() {
+        return null;
+    }
+
+    @Override
+    public int search(Card card) {
+        return 0;
+    }
+
+    @Override
+    public void newOrder() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
